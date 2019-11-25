342a5d1e974bec06ddc00ce57994c0a3551ae1e0
Yodargh <guille.cabj@gmail.com>
2019-11-25 05:50:16 -0300

    Cambio completo de la tarea
    luego de varias semanas sin programar cambie el codigo casi por completo completando la tarea correspondiente

--- tareas/clase-5Propio/tarea3.js
+++ tareas/clase-5Propio/tarea3.js
@@ -6,7 +6,79 @@
 // cada dato.
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
+
+let $BOTON_INGRESAR = document.querySelector('#calcular')
+let $RESULTADO_HORAS = document.querySelector('#resultado-hora')
+let $RESULTADO_MINUTOS = document.querySelector('#resultado-minutos')
+let $RESULTADO_SEGUNDOS = document.querySelector('#resultado-segundos')
+
+
+$BOTON_INGRESAR.onclick = function () {
+    let horasNode = document.querySelectorAll('.horas')
+    //Convierte en node el input del usuario de horas
+    let horasTotalesArray = Array.prototype.slice.call(horasNode)
+    //transforma el node a un array para poder sumarlos
+    let contenedorHoras = 0
+    //contiene el valor inicial para poder hacer un bucle y sumar el array correspondiente
+    let minutosNode = document.querySelectorAll('.minutos')
+    let minutosTotalesArray = Array.prototype.slice.call(minutosNode)
+    let contenedorMinutos = 0
+    let segundosNode = document.querySelectorAll('.segundos')
+    let segundosTotalesArray = Array.prototype.slice.call(segundosNode)
+    let contenedorSegundos = 0
+
+    // bucle para calcular los totales sumando el contenido del array
+    for (let i = 0; i < horasTotalesArray.length; i++) {
+        //el value es para agarrar el numero en cuestion sino tira nulo el bucle
+        contenedorHoras = contenedorHoras + Number(horasTotalesArray[i].value)
+    }
+
+    for (let i = 0; i < minutosTotalesArray.length; i++) {
+        contenedorMinutos = contenedorMinutos + Number(minutosTotalesArray[i].value)
+    }
+    for (let i = 0; i < segundosTotalesArray.length; i++) {
+        contenedorSegundos = contenedorSegundos + Number(segundosTotalesArray[i].value)
+
+    }
+
+    //bucle while para transformar minutos y segundos excedentes
+    while (contenedorSegundos > 59) {
+        contenedorSegundos = contenedorSegundos - 60
+        contenedorMinutos++
+    }
+
+    while (contenedorMinutos > 59) {
+        contenedorMinutos = contenedorMinutos - 60
+        contenedorHoras++
+    }
+
+
+    $RESULTADO_HORAS.value = contenedorHoras
+    $RESULTADO_MINUTOS.value = contenedorMinutos
+    $RESULTADO_SEGUNDOS.value = contenedorSegundos
+
+
+    console.log(contenedorHoras)
+    console.log(contenedorMinutos)
+    console.log(contenedorSegundos)
+
+    return false;
+}
+
+
+
+
+
+
+
+
+//
+// CODIGO VIEJO
+// NO SIRVE
+//
+
 /*
+
 const $BOTON_INGRESAR = document.querySelector('#boton-ingresar');
 
 
@@ -22,6 +94,7 @@ $BOTON_INGRESAR.onclick = function () {
 }
 */
 
+/*
 const $BOTON_INGRESAR = document.querySelector('#boton-ingresar');
 const $NODO_FORMULARIO = document.querySelector('form');
 
@@ -34,12 +107,12 @@ $BOTON_INGRESAR.onclick = function () {
         $CREAR_NUEVO_INPUT.id = `nuevo-formulario-${i}`
         $NODO_FORMULARIO.appendChild($CREAR_NUEVO_INPUT);
         $NODO_FORMULARIO.appendChild($CREAR_NUEVO_SEPARADOR);
-        
+
     }
     return false;
 
 }
-
+*/
 
 
 
